3b6a37fc6a8f8b81e7fa71143e979902a66e0c00
jenin <jeningeorge07@gmail.com>
2024-08-26 21:27:39 +0530

    php progs

--- act_7.php
+++ act_7.php
@@ -26,3 +26,33 @@ hi($a);
 echo $a;
 
 ?>
+
+<!-- example -->
+
+<?php
+
+function incre($i){
+    $i++;
+    return $i;
+}
+
+$i = 1;
+echo incre($i); //it will print 2
+echo $i; //it will print 1
+
+?>
+
+<!-- Reference -->
+
+<?php
+
+function inc(&$i){
+    $i++;
+    return $i;
+}
+
+$i = 1;
+echo inc($i); //it will print 2
+echo $i; //it will print 1
+
+?>
